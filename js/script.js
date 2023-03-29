5231a99c59e7b2e2983c612618e578b5f3aef1bd
francescovincidev <francescovinci2000@gmail.com>
2023-03-29 17:14:31 +0200

    Bonus 2 e 3

--- js/script.js
+++ js/script.js
@@ -4,6 +4,8 @@ createApp({
     data() {
         return {
             visibleImage: 0,
+            time: 3000,
+            interval: null,
             slides: [
                 {
                     image: 'img/01.webp',
@@ -34,6 +36,10 @@ createApp({
         };
     },
 
+    mounted() {
+        this.startAutoscroll();
+      },
+
     methods: {
         showNext() {
             if (this.visibleImage < this.slides.length - 1) {
@@ -57,32 +63,14 @@ createApp({
 
         },
 
+        startAutoscroll() {
+            this.interval = setInterval(this.showNext, this.time);
+          },
+
+        stopAutoscroll() {
+            clearInterval(this.interval);
+          },
+
     }
 
 }).mount("#app");
-
-const slides = [
-    {
-        image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
-
-console.log(slides);
